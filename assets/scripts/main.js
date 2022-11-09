3da0228e65c4a0d30007d010bc9087cc75bdcab9
afiqalghazali <afiqalghazali.edu@gmail.com>
2022-11-09 11:02:32 +0700

    adjustable some changes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -36,19 +36,3 @@ onload = function () {
 		sessionStorage.setItem("maximized", true);
 	}
 };
-
-/* Greetings */
-var today = new Date();
-var curHr = today.getHours();
-
-if (curHr < 12) {
-	$(".greetings").text("Good Morning");
-}
-
-if (curHr >= 12 && curHr < 18) {
-	$(".greetings").text("Good Afternoon");
-}
-
-if (curHr >= 18) {
-	$(".greetings").text("Good Evening");
-}
